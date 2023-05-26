8fa62ad8b4633fc5cabea6481628404f2e676a3e
Albert Ewaldo <ewaldoalbert90@gmail.com>
2023-05-26 17:49:42 +0700

    add todo

---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+function Heading() {
+    return(
+        <div className="text-center text-4xl font-bold">
+        ToDo List App
+        </div>
+    )
+}
+
+export default Heading;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Poppins: "Poppins"
+    },
+    extend: {
+      colors: {
+        transparentBlack: "rgba(0,0,0,0.85)",
+      }
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+}
+
